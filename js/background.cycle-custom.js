12ed82fb3dbb4a7ac75425f7fde59771e4980e9a
andersondepaula <61925789+andersondepaula@users.noreply.github.com>
2020-03-07 23:24:45 -0400

    Add files via upload

---
+++ js/background.cycle-custom.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+
+var fadeShow = $(".background").fadeShow({
+
+
+
+	correctRatio: true,
+
+
+
+	shuffle: true,
+
+
+
+	speed: 2500,
+
+
+
+	images: ['images/image_1.jpg',
+
+			 'images/image_2.jpg',
+
+			 'images/image_3.jpg',
+
+			 'images/image_4.jpg',
+			
+			]
+
+
+
+
+
+
+
+});
